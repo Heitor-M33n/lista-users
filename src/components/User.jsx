8b436709db44b224c806e82be0ac847984ff41ed
Heitor Ferreira da Silva <20241094010031@ifrn.local>
2026-08-21 11:35:54 -0300

    First commit

---
+++ src/components/User.jsx
@@ -0,0 +1,27 @@
+function User ({ image, firstName, lastName, age, gender, email, phone, companyName, companyTitle, adressCity}) {
+    return (
+        <>
+            <div className="cartao">
+                <div className="block1">
+                    <img src={image} alt="Foto da pessoa" />
+                    <div className="block2">
+                        <h2>{firstName} {lastName}</h2>
+                        <h3>{age} anos | {gender}</h3>
+                    </div>
+                </div>
+
+                <hr className="divider"/>
+
+                <div className="block1">
+                    <h3><strong>Email: </strong>{}</h3>
+                    <h3><strong>Telefone: </strong>{}</h3>
+                    <h3><strong>Empresa: </strong>{}</h3>
+                    <h3><strong>Cargo: </strong>{}</h3>
+                    <h3><strong>Cidade: </strong>{}</h3>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default User;
